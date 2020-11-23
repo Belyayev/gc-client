09e87619e0cc7a8b0568bf5dab627f4b6030bfad
Mike <4xgood@gmail.com>
2020-11-22 21:53:56 -0700

    before implementing Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-<h1>Groceries App</h1>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 }
 
